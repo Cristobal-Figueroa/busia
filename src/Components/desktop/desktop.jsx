b6ca6744156139c482eb9140945e05b00e15ddf5
AlejandroLiinares <alejandrolinaresnt@gmail.com>
2025-06-02 18:32:09 -0400

    Link a la flota

--- src/Components/desktop/desktop.jsx
+++ src/Components/desktop/desktop.jsx
@@ -5,7 +5,7 @@ function Desktop() {
   return (
     <div className="desktop-container">
       <h1>Escritorio</h1>
-      <div className="desktop-content">
+      <div src="/fleetVehicles" className="desktop-content">
         <div className="desktop-card">
           <h2>Flota</h2>
           <p>Administración de vehículos y conductores</p>
