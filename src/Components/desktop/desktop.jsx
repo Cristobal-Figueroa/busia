c6b0d974a3e7647c4c00986f83f66d16a075f7a2
AlejandroLiinares <alejandrolinaresnt@gmail.com>
2025-06-03 18:37:23 -0400

    Mejoras de enrutamiento

--- src/Components/desktop/desktop.jsx
+++ src/Components/desktop/desktop.jsx
@@ -5,9 +5,9 @@ function Desktop() {
   return (
     <div className="desktop-container">
       <h1>Escritorio</h1>
-      <div src="/fleetVehicles" className="desktop-content">
+      <div className="desktop-content">
         <div className="desktop-card">
-          <h2>Flota</h2>
+          <h2>Flota de Vehículos</h2>
           <p>Administración de vehículos y conductores</p>
         </div>
       </div>
